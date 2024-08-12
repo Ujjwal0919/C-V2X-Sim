ce1117b662a4dd9b9298954033cfefc1e522533d
Ujjwal0919 <sainiujju195@gmail.com>
2024-08-12 14:14:23 +0100

    Simulation working fine with all messages in FMS

--- FMS/FMS.py
+++ FMS/FMS.py
@@ -46,7 +46,7 @@ def handle_obu_request(connection):
     try:
         data = connection.recv(4096)
         request = json.loads(data.decode())
-        print("Received request:", request)
+        print("Received request type:", request['type'] + ' ' + request['sid'])
 
         if request['type'] == 'request_nonce':
             sid = request['sid']
@@ -57,11 +57,14 @@ def handle_obu_request(connection):
             nonce = os.urandom(16).hex()
             response = {'nonce': nonce, 'sid': sid}
             connection.sendall(json.dumps(response).encode())
+            print(f"Sending Nonce: {nonce}")
 
             # Wait for proof from OBU
             data = connection.recv(4096)
             proof_response = json.loads(data.decode())
             received_proof = proof_response['proof']
+            print(f"Got proof: {proof_response['proof']}")
+
 
             # Calculate expected proof
             expected_proof = hashes.Hash(hashes.SHA256())
@@ -70,7 +73,7 @@ def handle_obu_request(connection):
             calculated_proof = expected_proof.finalize().hex()
 
             if received_proof == calculated_proof:
-                print("Authentication successful")
+                print("Proof Matched and Authentication successful")
                 # Send FMS public key
                 fms_public_key, _ = fetch_fms_keys()
                 fms_public_key_pem = fms_public_key.public_bytes(
@@ -78,16 +81,19 @@ def handle_obu_request(connection):
                     format=serialization.PublicFormat.SubjectPublicKeyInfo
                 ).decode()
                 connection.sendall(json.dumps({'status': 'success', 'fms_public_key': fms_public_key_pem}).encode())
+                print("FMS Public Key Sent")
 
                 # Receive OBU public key and calculate shared secret
                 data = connection.recv(4096)
                 obu_public_key_response = json.loads(data.decode())
                 obu_public_key_pem = obu_public_key_response['obu_public_key']
                 obu_public_key = serialization.load_pem_public_key(obu_public_key_pem.encode())
+                print("Received OBU Public Key")
 
                 fms_private_key = fetch_fms_keys()[1]
                 shared_secret = fms_private_key.exchange(ec.ECDH(), obu_public_key).hex()
                 session_id = os.urandom(16).hex()
+                print("Shared Secret & session_id Calculated")
 
                 # Save OBU keys and shared secret
                 save_obu_keys(sid, obu_public_key_pem, shared_secret, session_id)
